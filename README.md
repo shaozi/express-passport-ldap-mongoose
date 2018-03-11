b6062ef790edeb5ba82d7f802590e4a466dca0de
Jingshao Chen <jingshaochen@gmail.com>
2018-03-11 16:31:57 -0400

    init version

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # express-passport-ldap-mongoose
 A library to use passport-ldapauth and local MongoDB to authenticate and save users
+
+### Requirement
+
+* node Express
+* Mongoose
+* Passport
+* Passport-ldapauth
+
+## Usage
+
+```
+const mongoose = require('mongoose')
+mongoose.Promise = Promise
+mongoose.connect(CONFIG.dburl, {
+  useMongoClient: true
+  /* other options */
+})
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+
+const assert = require('assert')
+const express = require("express")
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server, { path: `${apiPath}/socket.io` })
+const bodyParser = require('body-parser')
+const uuid = require('uuid/v1')
+const fs = require('fs')
+const spawn = require('child_process').spawn
+const winston = require('winston')
+winston.remove(winston.transports.Console)
+winston.add(winston.transports.Console, { 'timestamp': true })
+const User = require('../model/model').User
+
+
+const LdapAuth = require('express-passport-ldap-mongoose')
+
+var sessionMiddleWare = session({
+  secret: CONFIG.sessionSecret,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  resave: true,
+  saveUninitialized: true,
+  unset: 'destroy',
+  cookie: {
+    httpOnly: false,
+    maxAge: 1000 * 3600 * 24,
+    secure: false, // this need to be false if https is not used. Otherwise, cookie will not be sent.
+  }
+})
+
+
+const router = express.Router()
+
+
+// The order of the following middleware is very important!!
+router.use(bodyParser.json())
+router.use(sessionMiddleWare)
+LdapAuth.init(CONFIG.ldap.dn, CONFIG.ldap.url, router, (id, done) => {
+  User.findOne({ uid: id }).exec()
+    .then(user => {
+      if (!user) {
+        done(new Error(`Deserialize user failed. ${id} is deleted from local DB`))
+      } else {
+        done(null, user)
+      }
+    })
+}, (user, res) => {
+  User.findOneAndUpdate({ uid: user.uid }, user, { upsert: true, new: true }).exec()
+    .then(user => {
+      return res.json({ success: true, message: 'authentication succeeded', user: user.toObject() });
+    })
+})
+
+function ensureAuthenticated(req, res, next) {
+  if (!req.user) {
+    res.status(401).json({ success: false, message: "not logged in" })
+  } else {
+    next()
+  }
+}
+
+// all api need authentication
+router.all('/api/*', ensureAuthenticated)
+
+```
---
+++ index.js
@@ -0,0 +1,114 @@
+/**
+ * Passport LDAP authentication module
+ */
+
+const passport = require('passport')
+const LdapStrategy = require('passport-ldapauth')
+
+var _dn
+var _ldapurl
+var _findFunc
+var _insertFunc
+var _loginUrl
+var _logoutUrl
+var _router
+/**
+ * Set up ldap server information, callbacks, and express route.
+ * 
+ * @param {string} dn - ldap dn
+ * @param {string} ldapurl - ldap server url
+ * @param {object} router - express router
+ * @param {function} findFunc - function(id, done) to find the user in local db by id
+ * @param {function} insertFunc - function(user, res) to upsert user into local db
+ * @param {string} loginUrl - path to login page. Default: /login
+ * @param {string} logoutUrl - path to logout page. Default: /logout
+ */
+var init = function (dn, ldapurl, router, findFunc, insertFunc, loginUrl, logoutUrl) {
+  _dn = dn
+  _ldapurl = ldapurl
+  _router = router
+  _findFunc = findFunc
+  _insertFunc = insertFunc
+  _loginUrl = loginUrl || '/login'
+  _logoutUrl = logoutUrl || '/logout'
+
+  passport.use(new LdapStrategy((req, callback) => {
+    // Fetching things from database or whatever
+    // use x-www-form-urlencoded to fill in username and password
+    // req should be already urlencodedParser ed. body
+    // should be filled with username and password.
+    process.nextTick(() => {
+      var opts = {
+        server: {
+          url: _ldapurl,
+          bindDn: `uid=${req.body.username},${_dn}`,
+          bindCredentials: `${req.body.password}`,
+          searchBase: _dn,
+          searchFilter: `uid=${req.body.username}`,
+          reconnect: true
+        }
+      }
+      callback(null, opts)
+    })
+  },
+    (user, done) => {
+      return done(null, user)
+    }))
+
+  passport.serializeUser((user, done) => {
+    done(null, user.uid)
+  })
+  
+  passport.deserializeUser(_findFunc)
+
+  router.use(passport.initialize())
+  router.use(passport.session())
+  // error handling if deserialization failed (when user is deleted from db, but client still has user cookie, passport will fail all the time.)
+  router.use((err, req, res, next) => {
+    if (err) {
+      req.logout();
+      if (req.originalUrl == _loginUrl) {
+        next(); // never redirect login page to itself
+      } else {
+        if (req.flash && typeof (req.flash) === "function") {
+          req.flash("error", err.message);
+        }
+        res.redirect(_loginUrl);
+      }
+    } else {
+      next();
+    }
+  })
+  // login
+  router.post(_loginUrl, login)
+  router.get(_logoutUrl, function (req, res) {
+    req.logout();
+    res.redirect(_loginUrl);
+  })
+}
+
+
+/**
+ * Customized login authentication handler to send {success: true} 
+ * on successful authenticate, or {success: false} on failed authenticate
+ */
+var login = function (req, res, next) {
+  passport.authenticate('ldapauth', (err, user, info) => {
+    if (err) {
+      return next(err)
+    }
+    if (!user) {
+      res.status(401).json({ success: false, message: 'authentication failed' })
+    } else {
+      req.login(user, loginErr => {
+        if (loginErr) {
+          return next(loginErr);
+        }
+        _insertFunc(user, res)
+      })
+    }
+  })(req, res, next)
+}
+
+
+module.exports.init = init
